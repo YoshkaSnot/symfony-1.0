d9e8cfe05a81066025f2eb86fab286c94b876c9f
Yoshka <kravchenko@qtim.ru>
2023-11-25 21:45:34 +0300

    Обновлены конфиги проекта

--- public/index.php
+++ public/index.php
@@ -4,6 +4,14 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+/**
+ * @param array<string, mixed> $context
+ */
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    $kernel = new Kernel('dev', true);
+
+    if(isset($context['APP_ENV']) && isset($context['APP_DEBUG'])) {
+        $kernel = new Kernel((string) $context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    }
+    return $kernel;
 };
